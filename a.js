bdc061dc26f0b9444fccce8714c86e2ad739b660
Colin Goudie <colin.goudie@tributech.com.au>
2016-03-11 11:23:54 +1100

    Adding file that break

---
+++ a.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Highcharts = require('highcharts');
+require('highcharts/modules/exporting')(Highcharts);
+
+
+Highcharts.setOptions({
+  global: {
+    useUTC: false
+  }
+});
+
+
+var AdminDashboardController = function() {
+
+};
+
